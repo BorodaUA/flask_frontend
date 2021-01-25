b39d66478e788c075434ac8cced6481dd994082c
Andrii Kovalov <jfkwww1@gmail.com>
2021-01-25 13:51:35 +0000

    /blognews/{page_number} endpoint url_for fix

--- news/views/blognews.py
+++ news/views/blognews.py
@@ -78,22 +78,13 @@ def blog_news_page(page_number):
     try:
         api_request = requests.get(BlogNewsStoriesUrl)
     except requests.exceptions.ConnectionError:
-        api_request = None
-        api_response = None
-    if api_request:
-        if api_request.status_code == 200:
-            api_response = api_request.json()
-        elif api_request.status_code == 400:
-            abort(404)
-    else:
         abort(404)
     if api_request.status_code == 200:
+        api_response = api_request.json()
         resp = make_response(
             render_template(
                 "blognews_stories.html",
                 stories=api_response,
-                current_view_func="news.blog_news_page_func",
-                story_view_func="news.blognews_story_page_func",
             )
         )
         return resp
